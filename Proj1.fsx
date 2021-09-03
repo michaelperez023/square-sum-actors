45874f5ece2568ce1e79abefc6711bd0fd245495
michaelperez023 <michaelperez023@gmail.com>
2021-09-02 22:23:08 -0400

    First commit

---
+++ Proj1.fsx
@@ -0,0 +1,25 @@
+﻿//#time "on"
+#r "nuget: Akka.FSharp" 
+//#r "nuget: Akka.TestKit" 
+
+//open System
+open Akka.Actor
+//open Akka.Configuration
+//open Akka.FSharp
+//open Akka.TestKit
+
+let max = string (fsi.CommandLineArgs.GetValue 1)
+let length = string (fsi.CommandLineArgs.GetValue 2)
+let system = ActorSystem.Create("System")
+
+//type EchoServer =
+//    inherit Actor
+//    override x.OnReceive message =
+//        match message with
+//        | :? string as msg -> printfn "Hello %s" msg
+//        | _ ->  failwith "unknown message"
+//let echoServer = system.ActorOf(Props(typedefof<EchoServer>, Array.empty))
+
+printfn "max: %s" max
+printfn "length: %s" length
+
