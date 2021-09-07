db805688cf1dda83be019b4b1bcc099c4ffdfefc
michaelperez023 <michaelperez023@gmail.com>
2021-09-06 20:14:23 -0400

    Added boss and worker actors, and a function that takes as input an object of Input(n,k) type, where n and k are ints. The function returns true if the sum of squares from n .. k is a perfect square, and false otherwise.

--- Proj1.fsx
+++ Proj1.fsx
@@ -1,25 +1,53 @@
-﻿//#time "on"
+﻿#time "on"
 #r "nuget: Akka.FSharp" 
-//#r "nuget: Akka.TestKit" 
 
-//open System
+open System
 open Akka.Actor
-//open Akka.Configuration
-//open Akka.FSharp
-//open Akka.TestKit
+open Akka.FSharp
 
-let max = string (fsi.CommandLineArgs.GetValue 1)
-let length = string (fsi.CommandLineArgs.GetValue 2)
-let system = ActorSystem.Create("System")
+type Input = Input of int * int
 
-//type EchoServer =
-//    inherit Actor
-//    override x.OnReceive message =
-//        match message with
-//        | :? string as msg -> printfn "Hello %s" msg
-//        | _ ->  failwith "unknown message"
-//let echoServer = system.ActorOf(Props(typedefof<EchoServer>, Array.empty))
+let isSumOfSquaresPerfectSquare (Input(x, k)) : bool = 
+    let square x = x * x
+    let isPerfectSquare (x : int) : bool =
+        let y = round (sqrt (double x)) |> uint64
+        y * y = uint64 x
+    [ x .. x + k - 1 ] |> List.map square |> List.sum |> isPerfectSquare
 
-printfn "max: %s" max
-printfn "length: %s" length
+let WorkerActor (mailbox:Actor<_>) = 
+    let rec loop () = actor {
+        let! message = mailbox.Receive()
 
+        //match message with
+        //| Input(n, k) ->
+        //    printfn "processor received input %i and %i." n k
+        //| _ -> ()
+        return! loop ()
+    }
+    loop ()
+
+let BossActor (mailbox:Actor<_>) = 
+    let rec loop () = actor {
+        let! message = mailbox.Receive()
+
+        match message with
+        | Input(n, k) ->
+            printfn "processor received input %i and %i." n k
+            let check = Input(n, k) |> isSumOfSquaresPerfectSquare
+            if check then printfn "true" else printfn "false"
+        | _ -> ()
+        return! loop ()
+    }
+    loop ()
+
+match fsi.CommandLineArgs.Length with 
+| 3 ->
+    let max_n = fsi.CommandLineArgs.[1] |> int
+    let length_k = fsi.CommandLineArgs.[2] |> int
+
+    let system = ActorSystem.Create("System")
+    let bossRef = spawn system "boss" BossActor
+    bossRef <! Input(max_n, length_k)
+
+    ()
+| _ -> failwith "You must enter two numbers as input. i.e. 'dotnet fsi proj1.fsx 3 2'"
